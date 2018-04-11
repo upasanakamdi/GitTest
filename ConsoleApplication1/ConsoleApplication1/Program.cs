7f86644fd3fd0ecf2c1e3cebf0007e47167d08c6
Upasana <Matrica@UPASANA-PC>
2018-04-11 09:38:08 +0100

    commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Git Test");
+            Console.WriteLine("Git Test hjhjhj");
         }
     }
 }
